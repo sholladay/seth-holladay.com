85fc569596bf9b5272be39d6ffd4669f0fa04998
Seth Holladay <me@seth-holladay.com>
2017-12-22 08:34:52 -0500

    Improve thoroughness of tests

--- test.js
+++ test.js
@@ -15,8 +15,20 @@ if (!env.stripeSecretKey.startsWith('sk_test_')) {
     throw new Error('You must use a test secret key to avoid damaging live infrastructure');
 }
 
+const mockRequest = (app, option) => {
+    return app.inject({
+        method : 'GET',
+        url    : '/',
+        ...option
+    });
+};
+
 test('starts successfully', async (t) => {
     const app = await server(env);
     await t.notThrows(app.start());
+    const response = await mockRequest(app);
+    t.is(response.statusCode, 200);
+    t.is(response.headers['content-type'], 'text/html; charset=utf-8');
+    t.true(response.payload.includes('<title>Seth Holladay</title>'));
     await app.stop();
 });

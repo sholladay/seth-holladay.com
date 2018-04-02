b71f6d8da4aab0816c063594d4083ba93be5c4d0
Seth Holladay <me@seth-holladay.com>
2018-04-02 04:28:33 -0400

    Remove obsolete error supression in deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ URL="$(node_modules/.bin/now deploy -e STRIPE_PUBLIC_KEY=@dev-stripe-public-key 
 node_modules/.bin/await-url "$URL" --tries=200;
 node_modules/.bin/now alias set "$URL" dev.seth-holladay.com --team=sholladay --token=$ZEIT_TOKEN;
 # Attempt to remove old deployment, see https://github.com/zeit/now-cli/issues/691
-node_modules/.bin/now remove seth-holladay.com --yes --safe --team=sholladay --token=$ZEIT_TOKEN || true;
+node_modules/.bin/now remove seth-holladay.com --yes --safe --team=sholladay --token=$ZEIT_TOKEN;
